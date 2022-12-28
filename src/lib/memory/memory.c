614176d2cfe5d760409ab1d740132b2d3632fda1
KaloyanYosifov <kaloqn665@gmail.com>
2022-12-28 22:47:47 +0100

    Use less iterations to do the same initialization

--- src/lib/memory/memory.c
+++ src/lib/memory/memory.c
@@ -1,10 +1,11 @@
 #include "memory.h"
+#include <stdint.h>
 
 void* memset(void* ptr, int c, size_t size) {
-    char* c_ptr = (char*) ptr;
+    uint32_t* c_ptr = (uint32_t*) ptr;
 
-    for (size_t i = 0; i < size; i++) {
-        c_ptr[i] = (char) c;
+    for (size_t i = 0; i < size / sizeof(uint32_t); i++) {
+        c_ptr[i] = c;
     }
 
     return ptr;
