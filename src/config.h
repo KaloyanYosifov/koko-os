2cb376b44b3cd097fcfcd922b9f3719668ecc4ab
KaloyanYosifov <kaloqn665@gmail.com>
2022-12-28 21:51:00 +0100

    Use human readable interrupt amount

--- src/config.h
+++ src/config.h
@@ -1,8 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// 512 total interrupts
-#define KERNEL_TOTAL_INTERRUPTS 0x200
+#define KERNEL_TOTAL_INTERRUPTS 512
 #define KERNEL_CODE_SELETOR 0x08;
 #define KERNEL_DATA_SELETOR 0x10;
 
